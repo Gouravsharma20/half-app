e605303c8e5364dc39be6d026f81f72e2ae098de
Gourav <gouravsharma20a@gmail.com>
2024-07-05 13:27:02 +0530

    added categories of item as lunch, dinner and breakfast

--- app/src/main/java/wu/tutorials/half_app/MainActivity.kt
+++ app/src/main/java/wu/tutorials/half_app/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
@@ -35,12 +37,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import wu.tutorials.half_app.ui.theme.Half_appTheme
+import java.util.Locale.Category
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            HomeScreen()
             val context = LocalContext.current
             Column(modifier = Modifier
                 .fillMaxWidth()
@@ -74,24 +78,44 @@ class MainActivity : ComponentActivity() {
                     border = BorderStroke(1.dp,Color.Red), modifier = Modifier.padding(top = 10.dp)) {
                     Text(text = "submit")
                 }
+                Row (modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+                    , horizontalArrangement = Arrangement.SpaceEvenly
+                )
+
+                {
+                    categories.forEach{
+                        MenuCategory(category = it)
+                    }
+                }
             }
+
+
         }
 
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MenuCategory(category: String) {
+    Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.Gray)) {
+        Text(text = category)
+
+    }
 }
 
-@Preview(showBackground = true)
+
+val categories = listOf<String>(
+    "breakfast",
+    "lunch",
+    "dinner"
+
+)
+
 @Composable
-fun GreetingPreview() {
-    Half_appTheme {
-        Greeting("Android")
+fun HomeScreen() {
+    Scaffold {
     }
+    
 }
