03e4dc69e849e6e1cee474ea2734b04ec7e67851
Gourav <gouravsharma20a@gmail.com>
2024-07-05 10:50:39 +0530

    we have added the top pannel of our app successfully

---
+++ app/src/main/java/wu/tutorials/half_app/MainActivity.kt
@@ -0,0 +1,87 @@
+package wu.tutorials.half_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import wu.tutorials.half_app.ui.theme.Half_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF495E57))
+                .padding(top = 25.dp, start = 15.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.Start
+
+                ) {
+                Text(text = stringResource(id = R.string.top_heading), fontSize = 32.sp, color = Color(0xFFF4CE14), modifier = Modifier.padding(top=10.dp))
+                Text(text = stringResource(id = R.string.tag_line), fontSize = 24.sp, color = Color.White, modifier = Modifier.padding(top = 5.dp,bottom=10.dp))
+
+
+
+                Row (modifier = Modifier.fillMaxWidth().padding(5.dp), horizontalArrangement = Arrangement.Start){
+
+                    Text(text = stringResource(id = R.string.discription), color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.width(200.dp).height(100.dp))
+                    Image(painter = painterResource(id = R.drawable.littlelemon), contentDescription = "", modifier = Modifier.width(220.dp).height(110.dp).clip(
+                        RoundedCornerShape(200.dp)
+                    ))
+                }
+                Button(onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(Color.Gray),
+                    border = BorderStroke(1.dp,Color.Red), modifier = Modifier.padding(top = 10.dp)) {
+                    Text(text = "submit")
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Half_appTheme {
+        Greeting("Android")
+    }
+}
