9a59173a4fd8333dde6f2f98c3d7bd0fce2ef660
Gourav <gouravsharma20a@gmail.com>
2024-07-10 13:55:19 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/java/wu/tutorials/half_app/MainActivity.kt

---
+++ app/src/main/java/wu/tutorials/half_app/ui/theme/DrawerPanel.kt
@@ -0,0 +1,25 @@
+package wu.tutorials.half_app.ui.theme
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun DrawerPanel(scaffoldState: ScrollState,scope:CoroutineScope) {
+    List(10) { Text(text = "Item -$it", modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp))}
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(imageVector =Icons.Default.ExitToApp, contentDescription = "close icon")
+
+    }
+
+}
